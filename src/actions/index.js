b76ed963947df3b4ce21562f1aa0e79d4d5a7a39
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-19 07:50:15 -0400

    basic reducer set up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,26 @@
 import axios from 'axios';
 
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START'
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS'
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL'
+
 //Task List:
+export const getPokemon = () => dispatch => {
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
+  dispatch({ type: FETCH_SMURFS_START })
+
+  axios.get()
+    .then(res => {
+      dispatch({
+        type: FETCH_SMURFS_SUCCESS,
+        payload: res.data
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_SMURFS_FAIL, payload: err
+      })
+    })
+}
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
 //3. Add a standard action that allows us to set the value of the error message slice of state.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,36 @@
-
-export const initialState = {
-}
-
-const reducer = ()=>{
-}
-
-export default reducer;
+//2. Add in the arguments needed to complete a standard reducer function.
+import {
+  FETCH_SMURFS_START
+} from '../actions'
 
 //Task List:
 //1. Adds the following state values into the initialState:
 //  - an array of smurfs
 //  - a boolean indicating if the app is loading
 //  - a string indicating a possible error message
+export const initialState = {
+  smurfs: [],
+  error: '',
+  isFetching: false
+}
 
-//2. Add in the arguments needed to complete a standard reducer function.
-//3. Add in a reducer case to accomidate the start of a smurf fetch.
+const reducer = (state = initialState, { type, payload })=>{
+  switch (type) {
+    //3. Add in a reducer case to accomidate the start of a smurf fetch.
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        isFetching: true,
+        error: ''
+      }
+
+    default: return state;
+  }
+}
+
+export default reducer;
+
+
 //4. Add in a reducer case to accomidate the successful smurf api fetch.
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
