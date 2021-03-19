741502f783396ec65aac453f191bab92d2eeb5cb
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-19 07:39:16 -0400

    set up provider, store, and thunk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//Task List:
+//1. Add in all necessary components and libary methods.
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
 import "./index.css";
 import App from "./App";
@@ -9,12 +15,13 @@ worker.start();
 
 const rootElement = document.getElementById("root");
 
+//2. Create a store that includes thunk and logger middleware support.
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-    <App />, 
+//3. Wrap the App component in a react-redux Provider element.
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
-
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
